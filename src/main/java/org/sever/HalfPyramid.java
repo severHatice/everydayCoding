fbbdf614e21442d35bce5cf111c8ac3e6d25230b
severHatice <95497761+severHatice@users.noreply.github.com>
2022-10-12 23:21:40 +0200

    halfpyramid

---
+++ src/main/java/org/sever/HalfPyramid.java
@@ -0,0 +1,29 @@
+package org.sever;
+
+public class HalfPyramid {
+
+    public static void main(String[] args) {
+		/*
+	  	Ask user to enter the number of lines of a half pyramid.
+	  	Type a program to create the half pyramid.
+  	    For example if the number of lines is 5, the pyramid will be like;  *
+																		    * *
+																		    * * *
+																		    * * * *
+																		    * * * * *
+    */
+
+        int rows=5;
+        for(int i=1; i<=rows; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
