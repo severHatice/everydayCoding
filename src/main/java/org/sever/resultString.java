83dfaf364f41b814e5924c3bd3065cbcdcea748f
severHatice <95497761+severHatice@users.noreply.github.com>
2022-11-02 00:13:53 +0100

    string

---
+++ src/main/java/org/sever/resultString.java
@@ -0,0 +1,27 @@
+package org.sever;
+
+public class resultString {
+
+public static void main(String[] args) {
+        String s="abc";
+        s="xyz";
+String ss="abc";
+ss="xyz";
+if(ss.equals(s)){
+        System.out.println(true);
+}else{
+        System.out.println(false);
+}
+        String r = "Polish";
+        String t = "Polish";
+
+        if(r==t)
+        {
+        System.out.println(true);
+        }
+
+
+        System.out.println(s);
+        }
+
+        }
